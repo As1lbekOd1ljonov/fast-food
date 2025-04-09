9ca562004ad7cfd635cd7d28da814f356f9d807b
As1lbekOd1ljonov <aslbekoo176@gmail.com>
2025-04-09 20:29:22 +0500

    vazifa

--- config/main/views.py
+++ config/main/views.py
@@ -1,39 +1,45 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
-from rest_framework.permissions import IsAuthenticated
+
 
 from .models import Category, Product, Order
+from .permissions import Permission
 from .serializer import  CategorySerializers, ProductSerializers, OrderSerializers
 
 
 class CategoryApiView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializers
+    permission_classes = [Permission]
 
 
 class CategoryDetailApiView(RetrieveUpdateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializers
-    permission_classes = [IsAuthenticated]
+    permission_classes = [Permission]
 
 
 
 class ProductApiView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
+    permission_classes = [Permission]
+
 
 
 class ProductDetailApiView(RetrieveUpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
-    permission_classes = [IsAuthenticated]
+    permission_classes = [Permission]
+
 
 
 class OrderApiView(ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializers
+    permission_classes = [Permission]
 
 
 class OrderDetailApiView(RetrieveUpdateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializers
-    permission_classes = [IsAuthenticated]
+    permission_classes = [Permission]
